7f757953fc5051960c448b45289241fb0899ac7b
Darbanho <aa_darbanho08@example.com>
2025-04-30 20:44:58 +0300

    second_c

--- collect_files.sh
+++ collect_files.sh
@@ -1,19 +1,6 @@
-if [ "$#" -ne 2 ]; then
+#!/bin/bash
+if [[ $# -ne 2 ]]; then
   echo "Usage: $0 input_dir output_dir"
   exit 1
 fi
-
-input_dir="$1"
-output_dir="$2"
-
-if [ ! -d "$input_dir" ]; then
-  echo "Input directory does not exist."
-  exit 1
-fi
-
-mkdir -p "$output_dir"
-
-find "$input_dir" -type f | while read -r file; do
-  filename=$(basename "$file")
-  cp "$file" "$output_dir/$filename"
-done
+python3 collect_files.py "$1" "$2"
