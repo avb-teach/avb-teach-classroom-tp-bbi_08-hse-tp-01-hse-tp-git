459af864375287b873c905d2accda6518115d1f6
Darbanho <aa_darbanho08@example.com>
2025-05-01 21:19:14 +0300

    maybe this

--- collect_files.py
+++ collect_files.py
@@ -15,17 +15,19 @@ def collect_file_dirrs(in_dirr, out_dirr, max_depth=None):
         if max_depth is not None and depth > max_depth:
             continue
 
-        rel_path = os.path.relpath(root, in_dirr)
-        aim_dir = os.path.join(out_dirr, rel_path) if max_depth is not None else out_dirr
-        os.makedirs(aim_dir, exist_ok=True)
+        if max_depth is not None:
+            rel_path = os.path.relpath(root, in_dirr)
+            aim_dir = os.path.join(out_dirr, rel_path)
+            os.makedirs(aim_dir, exist_ok=True)
+        else:
+            aim_dir = out_dirr
+            os.makedirs(aim_dir, exist_ok=True)
 
         for file in files:
             src = os.path.join(root, file)
-
-            if max_depth is not None:
-                dst = os.path.join(aim_dir, file)
-            else:
-                dst = os.path.join(aim_dir, file)
+            dst = os.path.join(aim_dir, file)
+            
+            if not max_depth:
                 name, ext = os.path.splitext(file)
                 count = 1
                 while os.path.exists(dst):
