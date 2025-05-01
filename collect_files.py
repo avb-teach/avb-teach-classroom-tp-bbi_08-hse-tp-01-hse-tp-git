a620400ed8709b31df707c6cfa8aca185e82d4c5
Darbanho <aa_darbanho08@example.com>
2025-05-01 20:25:16 +0300

    fin_mark2_hope_last_c

--- collect_files.py
+++ collect_files.py
@@ -6,25 +6,25 @@ def glubb(base, path):
     rel_path = os.path.relpath(path, base)
     return rel_path.count(os.sep) + 1
 
-
 def collect_file_dirrs(in_dirr, out_dirr, max_depth=None):
     if not os.path.isdir(in_dirr):
         sys.exit(0)
-
-    os.makedirs(out_dirr, exist_ok=True)
 
     for root, dirrs, file_dirrs in os.walk(in_dirr):
         depth = glubb(in_dirr, root)
         if max_depth is not None and depth > max_depth:
             continue
+        rel_path = os.path.relpath(root, in_dirr)
+        aim_dir = os.path.join(out_dirr, rel_path)
+        os.makedirs(aim_dir, exist_ok=True)
 
         for file_dirr in file_dirrs:
             src = os.path.join(root, file_dirr)
-            dst = os.path.join(out_dirr, file_dirr)
+            dst = os.path.join(aim_dir, file_dirr)
             count = 1
             name, ext = os.path.splitext(file_dirr)
             while os.path.exists(dst):
-                dst = os.path.join(out_dirr, f"{name}_{count}{ext}")
+                dst = os.path.join(aim_dir, f"{name}_{count}{ext}")
                 count += 1
 
             shutil.copy(src, dst)
@@ -41,6 +41,4 @@ if __name__ == "__main__":
             max_depth = int(sys.argv[i + 1])
         except:
             max_depth = None
-
-
     collect_file_dirrs(in_dirr, out_dirr, max_depth)
